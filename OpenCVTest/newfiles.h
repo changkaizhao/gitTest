1daf7389f7d2c05d6b98ebea9ed7f78e3f6625f1
changkaizhao <changkaizhao1006@gmail.com>
2015-06-23 09:37:42 +0800

    add files on dev

---
+++ OpenCVTest/newfiles.h
@@ -0,0 +1,13 @@
+//
+//  newfiles.h
+//  OpenCVTest
+//
+//  Created by 赵常凯 on 15/6/23.
+//  Copyright (c) 2015年 赵常凯. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface newfiles : NSObject
+
+@end
---
+++ OpenCVTest/newfiles.m
@@ -0,0 +1,13 @@
+//
+//  newfiles.m
+//  OpenCVTest
+//
+//  Created by 赵常凯 on 15/6/23.
+//  Copyright (c) 2015年 赵常凯. All rights reserved.
+//
+
+#import "newfiles.h"
+
+@implementation newfiles
+
+@end
